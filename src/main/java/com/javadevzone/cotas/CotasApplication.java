0cdebc359e27b3f2b89ccfa7f250affad87f9ed6
Paulo Jr <javadevzn@gmail.com>
2019-12-04 23:23:15 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/com/javadevzone/cotas/CotasApplication.java
@@ -0,0 +1,13 @@
+package com.javadevzone.cotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javadevzone/cotas/CotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadevzone.cotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
