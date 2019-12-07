33e561173d49a7e27b1fa2da1e713c7d7c98b4d1
Paulo Jr <javadevzn@gmail.com>
2019-12-07 20:23:12 -0300

    Iniciando os testes por TDD

--- src/test/java/com/javadevzone/cotas/CotasApplicationTests.java
+++ src/test/java/com/javadevzone/cotas/CotasApplicationTests.java
@@ -8,6 +8,7 @@ class CotasApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
---
+++ src/test/java/com/javadevzone/cotas/services/CotizadorServiceTest.java
@@ -0,0 +1,16 @@
+package com.javadevzone.cotas.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.util.Assert;
+
+@ExtendWith(MockitoExtension.class)
+public class CotizadorServiceTest {
+
+    @Test
+    public void test() {
+        Assert.isTrue(true, "Verdadeiro");
+    }
+
+}
