b6f348abe70c487ea4175706e5fb1404e691c41e
Luiz Leite Oliveira <luiz.leite@hotmart.com>
2020-02-17 09:12:49 -0300

    criando mais testes para o sistema de cotas

--- src/test/java/com/javadevzone/cotas/services/InvestmentServiceTest.java
+++ src/test/java/com/javadevzone/cotas/services/InvestmentServiceTest.java
@@ -1,7 +1,6 @@
 package com.javadevzone.cotas.services;
 
-import com.javadevzone.cotas.entity.Wallet;
-import com.javadevzone.cotas.entity.WalletHistory;
+import com.javadevzone.cotas.entity.*;
 import com.javadevzone.cotas.repository.AssetHistoryRepository;
 import com.javadevzone.cotas.repository.AssetRepository;
 import com.javadevzone.cotas.repository.InvestmentRepository;
@@ -13,9 +12,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 import static java.math.BigDecimal.valueOf;
+import static java.util.Collections.singletonList;
+import static java.util.Optional.of;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class InvestmentServiceTest {
@@ -31,13 +35,30 @@ class InvestmentServiceTest {
 
 
     @Test
-    public void given_new_quota_without_history_should_return_zero_profitable() {
+    public void given_new_quota_without_history_should_return_negative_profitable() {
         Wallet myWallet = Wallet.builder().id(1L).build();
         WalletHistory walletHistoryMock = WalletHistory.builder().quota(valueOf(1.189716))
                 .totalQuotas(valueOf(1692)).wallet(myWallet).build();
 
         BigDecimal result = investmentService.calculateInvestmentsProfitability(walletHistoryMock, LocalDate.now());
         assertEquals(0, BigDecimal.valueOf(-1).compareTo(result));
+
+    }
+
+    @Test
+    public void given_history_and_assets_will_calculate_the_profitability() {
+        Wallet myWallet = Wallet.builder().id(1L).build();
+        WalletHistory walletHistoryMock = WalletHistory.builder().quota(valueOf(1.189716))
+                .totalQuotas(valueOf(1692)).wallet(myWallet).build();
+        Asset jhsfTick = Asset.builder().ticket("JHSF3").build();
+        when(investmentRepository.getQuantityByWalletAndAssetAndDateBefore(myWallet, jhsfTick, LocalDate.now()))
+                .thenReturn(of(200L));
+        when(assetHistoryRepository.findByAssetAndDate(any(Asset.class), any(LocalDate.class)))
+                .thenReturn(of(AssetHistory.builder().value(new BigDecimal("6.82")).asset(jhsfTick).build()));
+        when(assetRepository.findInvestedAssetsByDate(any(Wallet.class), any(LocalDate.class))).thenReturn(of(singletonList(jhsfTick)));
+
+        BigDecimal result = investmentService.calculateInvestmentsProfitability(walletHistoryMock, LocalDate.now());
+        assertEquals(0, BigDecimal.valueOf(-0.32240419).compareTo(result));
 
     }
 
