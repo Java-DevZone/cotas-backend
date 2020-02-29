166e9d3ae5bf54a611d83be2ebfcd8a99a9b397f
Paulo Jr <javadevzn@gmail.com>
2020-02-29 15:50:45 -0300

    Resolvendo issue #8 ao adicionar investimentos

--- src/main/java/com/javadevzone/cotas/entity/Investment.java
+++ src/main/java/com/javadevzone/cotas/entity/Investment.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 
 
 @Entity
-@Builder
+@Builder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
--- src/test/java/com/javadevzone/cotas/services/InvestmentServiceTest.java
+++ src/test/java/com/javadevzone/cotas/services/InvestmentServiceTest.java
@@ -1,24 +1,29 @@
 package com.javadevzone.cotas.services;
 
 import com.javadevzone.cotas.entity.*;
+import com.javadevzone.cotas.exceptions.WalletNotFoundException;
 import com.javadevzone.cotas.repository.AssetHistoryRepository;
 import com.javadevzone.cotas.repository.AssetRepository;
 import com.javadevzone.cotas.repository.InvestmentRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.List;
 
+import static com.javadevzone.cotas.entity.enums.AssetType.ACAO;
 import static java.math.BigDecimal.valueOf;
 import static java.util.Collections.singletonList;
 import static java.util.Optional.of;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,7 +41,7 @@ class InvestmentServiceTest {
 
     @Test
     public void given_new_quota_without_history_should_return_negative_profitable() {
-        Wallet myWallet = Wallet.builder().id(1L).build();
+        Wallet myWallet = getWallet();
         WalletHistory walletHistoryMock = WalletHistory.builder().quota(valueOf(1.189716))
                 .totalQuotas(valueOf(1692)).wallet(myWallet).build();
 
@@ -47,7 +52,7 @@ class InvestmentServiceTest {
 
     @Test
     public void given_history_and_assets_will_calculate_the_profitability() {
-        Wallet myWallet = Wallet.builder().id(1L).build();
+        Wallet myWallet = getWallet();
         WalletHistory walletHistoryMock = WalletHistory.builder().quota(valueOf(1.189716))
                 .totalQuotas(valueOf(1692)).wallet(myWallet).build();
         Asset jhsfTick = Asset.builder().ticket("JHSF3").build();
@@ -59,6 +64,43 @@ class InvestmentServiceTest {
 
         BigDecimal result = investmentService.calculateInvestmentsProfitability(walletHistoryMock, LocalDate.now());
         assertEquals(0, BigDecimal.valueOf(-0.32240419).compareTo(result));
+    }
+
+    @Test
+    public void given_a_new_investment_should_insert_in_the_wallet() {
+        Asset asset = getSingleAsset();
+        Wallet wallet = getWallet();
+        Investment investment = Investment.builder()
+                .asset(asset).quantity(500L).value(new BigDecimal("25.5")).wallet(wallet)
+                .build();
+        ArgumentCaptor<Investment> managedInvestment = ArgumentCaptor.forClass(Investment.class);
+
+        investmentService.addInvestment(investment);
+
+        verify(investmentRepository)
+                .save(managedInvestment.capture());
+
+        assertThat(managedInvestment.getValue().getDate())
+                .isEqualTo(LocalDate.now());
+    }
+
+    @Test
+    public void given_a_new_investment_should_have_a_wallet_or_throws_wallet_notfound_exception() {
+        Asset asset = getSingleAsset();
+        Investment investment = Investment.builder().asset(asset).quantity(500L).value(new BigDecimal("25.5")).build();
+
+        assertThatThrownBy(() -> investmentService.addInvestment(investment))
+                .isInstanceOf(WalletNotFoundException.class);
+    }
+
+    private Wallet getWallet() {
+        return Wallet.builder()
+                .id(1L).name("PJ Investing Club")
+                .build();
+    }
+
+    private Asset getSingleAsset() {
+        return Asset.builder().type(ACAO).ticket("PETR4").build();
     }
 
 }
